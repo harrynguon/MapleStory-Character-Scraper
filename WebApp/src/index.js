9e4dd18736db81242c8d83808f737f112e4ce43a
Harry <github.harrynguon@fairfrozen.com>
2023-09-16 16:35:44 +1200

    Added React Router DOM and a link to character directory app

--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<ul>
+				<li>
+					<Link to={`character_directory`}>MapleStory Character Directory</Link>
+				</li>
+			</ul>
+		),
+	},
+	{
+		path: "character_directory",
+		element: <App />,
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
